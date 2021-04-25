3ed2c1092278342fbc1a28e50146e07598fc0f1c
czert <pyhovoleg31@gmail.com>
2021-04-25 22:04:32 +0500

    OTHER: added earth engine JS source

---
+++ other/earth-engine-script.js
@@ -0,0 +1,36 @@
+var default_projection = ee.Projection({
+    crs: 'EPSG:3857',
+  })
+  .atScale(50);
+
+var pixels = ee.Image.pixelLonLat()
+.clip(geometry)
+.reproject({
+crs: default_projection
+});
+print(pixels.sample());
+
+var land_cover = ee.Image("COPERNICUS/Landcover/100m/Proba-V-C3/Global/2019")
+.select('discrete_classification')
+.clip(geometry)
+.reproject({
+crs: default_projection
+});
+print(land_cover.sample());                
+
+var elevation = ee.Image('CGIAR/SRTM90_V4')
+.clip(geometry)
+.reproject({
+crs: default_projection
+});
+print(elevation.sample());                
+
+Map.addLayer(elevation, {min: 200, max: 300}, "Elevation", true, 0.5);
+Map.addLayer(land_cover, {}, "Land Cover", true, 0.5);
+
+var image = ee.Image.cat(pixels, elevation, land_cover);
+
+Export.table.toDrive({
+collection: image.sample(),
+fileNamePrefix: "image_data"
+});
